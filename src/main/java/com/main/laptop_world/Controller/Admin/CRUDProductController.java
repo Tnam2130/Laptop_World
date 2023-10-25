5eee471ecdbd7caad2d690b49f4b2673998815ce
ASUS <thanhduong.120503@gmail.com>
2023-10-25 17:09:00 +0700

    as
    as

--- src/main/java/com/main/laptop_world/Controller/Admin/CRUDProductController.java
+++ src/main/java/com/main/laptop_world/Controller/Admin/CRUDProductController.java
@@ -1,15 +1,23 @@
 package com.main.laptop_world.Controller.Admin;
 
 import com.main.laptop_world.Entity.Category;
+import com.main.laptop_world.Entity.ProductImages;
 import com.main.laptop_world.Entity.Products;
 import com.main.laptop_world.Repository.CategoryRepository;
 import com.main.laptop_world.Repository.ProductRepository;
+import com.main.laptop_world.Repository.productImagesRepository;
 import com.main.laptop_world.Services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -18,13 +26,17 @@ public class CRUDProductController {
     ProductService productService;
     CategoryRepository categoryRepository;
 
-    public CRUDProductController(ProductRepository productRepository, ProductService productService, CategoryRepository categoryRepository) {
+    productImagesRepository productImagesRepository;
+    public CRUDProductController(ProductRepository productRepository,
+                                 ProductService productService,
+                                 CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
         this.productService = productService;
         this.categoryRepository = categoryRepository;
+
     }
 
-    @GetMapping("/admin/products")
+    @RequestMapping("/admin/products")
     public String getProductForm(Model model) {
         List<Products> productList = productRepository.findAll();
         List<Category> categories = categoryRepository.findAll();
@@ -33,21 +45,19 @@ public class CRUDProductController {
         model.addAttribute("product",new Products());
         return "admin/quanLySanPham";
     }
-//    @GetMapping( "/admin/products/add")
-//    public String addProduct(Model model, )
-//    {
-//        Products products = new Products();
-//
-//        model.addAttribute("user", products);
-//
-//        return "admin/quanLySanPham";
-//    }
 
     @PostMapping("/admin/products/add")
-    public String save(@ModelAttribute("product") Products products) {
+    public String save(@ModelAttribute("product") Products products, Model model,@RequestParam MultipartFile url) throws IOException {
+
+        ProductImages productImages = new ProductImages();
+        model.addAttribute("productImages",productImages);
+        productImages.setUrl(url.getBytes());
+        products.getImages().add(productImages);
         productRepository.save(products);
         return "redirect:/admin/products";
     }
+
+
 
     @GetMapping(value = "/admin/products/delete/{id}")
     public String delete(@PathVariable Long id) {
