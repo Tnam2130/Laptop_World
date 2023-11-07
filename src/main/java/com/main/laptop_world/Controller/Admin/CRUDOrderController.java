1dcb8d0b9685aa4fa5a1650b752ec3ddc20c5b06
ASUS <thanhduong.120503@gmail.com>
2023-11-07 20:13:10 +0700

    as
    a

--- src/main/java/com/main/laptop_world/Controller/Admin/CRUDOrderController.java
+++ src/main/java/com/main/laptop_world/Controller/Admin/CRUDOrderController.java
@@ -1,25 +1,53 @@
 package com.main.laptop_world.Controller.Admin;
 
+import com.main.laptop_world.Entity.Category;
 import com.main.laptop_world.Entity.Order;
+import com.main.laptop_world.Repository.OrderItemRepository;
+import com.main.laptop_world.Repository.OrderRepository;
 import com.main.laptop_world.Services.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 public class CRUDOrderController {
     OrderService orderService;
+    OrderRepository orderRepository;
 
-    public CRUDOrderController(OrderService orderService) {
+    public CRUDOrderController(OrderService orderService,
+                               OrderRepository orderRepository) {
         this.orderService = orderService;
+        this.orderRepository = orderRepository;
     }
 
     @GetMapping("/admin/order")
     public String getOrderForm(Model model) {
         List<Order> orderList = orderService.findAll();
-        model.addAttribute("orderList", orderList);
+        model.addAttribute("orders", orderList);
         return "admin/QuanLyDonHang";
     }
-}
+
+
+    @GetMapping(value = "/admin/order/delete/{id}")
+    public String deleteColor(@PathVariable Long id, RedirectAttributes ra) {
+        ra.addFlashAttribute("message", "Delete successfully");
+        orderRepository.deleteById(id);
+        return "redirect:/admin/order";
+    }
+
+    @GetMapping("/admin/order/update/id={id}")
+    public String getUpdateCategory(@PathVariable("id") Long id, Model model, @ModelAttribute Order order) {
+        Order orders = orderRepository.getById(id);
+        model.addAttribute("orders", orders);
+        return "admin/Update/updateOrder";
+    }
+    @PostMapping("/admin/order/update")
+    public String updateCategory(@RequestParam Long id, @RequestParam String status, RedirectAttributes ra) {
+        ra.addFlashAttribute("message", "Update successfully");
+        orderService.updateOrder(id, status);
+        return "redirect:/admin/order";
+    }
+    }
--- src/main/java/com/main/laptop_world/Repository/OrderRepository.java
+++ src/main/java/com/main/laptop_world/Repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.main.laptop_world.Repository;
 
 import com.main.laptop_world.Entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUserId(Long userId);
+
+    @Query("SELECT new Order (MONTH(d.createdAt), SUM(d.total)) FROM Order d GROUP BY MONTH(d.createdAt)")
+    List<Order> tinhDoanhThuTheoThang();
 }
